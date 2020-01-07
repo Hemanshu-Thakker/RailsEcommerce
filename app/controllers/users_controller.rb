410a514d53b6c9398b4ba03f53b2e6833d67bd85
Hemanshu Thakker <hemanshupthakker@gmail.com>
2020-01-07 15:00:31 +0530

    Mailer functionalities included

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -92,6 +92,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:username, :password, :balance)
+      params.require(:user).permit(:username, :password, :balance, :email)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_03_053549) do
+ActiveRecord::Schema.define(version: 2020_01_07_060854) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_01_03_053549) do
     t.integer "balance"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
