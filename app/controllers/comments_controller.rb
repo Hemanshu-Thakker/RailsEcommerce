7ed218eff6f7fb346a1a02d0a98992e77941388d
Hemanshu Thakker <hemanshupthakker@gmail.com>
2020-02-07 18:31:55 +0530

    comment errors reduced

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,10 +8,13 @@ class CommentsController < ApplicationController
 	# end
 	def create
 		@item= Item.find(params[:item_id])
-		@order= Order.new
 		@average_rating= Comment.findAverageRating(@item)
+		@cart=current_user.cart
 		@comment= Comment.create(user_id: current_user.id ,item_id: @item.id ,body: comment_params["body"], rating: comment_params["rating"]) 
 		if comment_params["body"]==nil or comment_params["body"]==""
+			@comment.destroy
+			render 'items/show'
+		elsif comment_params["rating"]==nil or comment_params["rating"]==""
 			@comment.destroy
 			render 'items/show'
 		elsif current_user==@item.user
